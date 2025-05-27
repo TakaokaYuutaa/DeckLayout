c1a1e011d5c31b53c6404a48002538e67672d8ce
Taokata_yu <dorasuke5897@gmail.com>
2025-05-27 16:08:10 +0900

    CardVariableSetingスクリプト_作成

--- Assets/Scripts/CardvariableSeting.cs
+++ Assets/Scripts/CardvariableSeting.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class CardvariableSeting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Parameter _para;
+    [SerializeField] GameObject[] _card;
+    public class Parameter
     {
-        
+        int cost, hitPoint;
+        int costLeast = 1, costHighest = 5;
+        int hitPointLeast = 10, hitPointHighest = 255;
+        public int SetCost()
+        {
+            int cost=Random.Range(costLeast, costHighest + 1);
+            return cost;
+        }
+        public int SetHitPoint()
+        {
+            int HP =Random.Range(hitPointLeast, hitPointHighest + 1);
+            return HP;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
